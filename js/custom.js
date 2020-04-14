91bfbd3a8e1fa32a66fd72777f37e431c3c2f79d
Talha Zahid <talha.zahid86@gmail.com>
2020-04-14 19:46:54 +0500

    no message

--- js/custom.js
+++ js/custom.js
@@ -259,3 +259,21 @@
 
 
 
+
+
+
+
+var navTrigger = document.getElementsByClassName('nav-trigger')[0],
+    body = document.getElementsByTagName('body')[0];
+
+navTrigger.addEventListener('click', toggleNavigation);
+
+function toggleNavigation(event) {
+  event.preventDefault();
+  body.classList.toggle('dashboard-right-section-slide');
+}
+
+
+
+
+
